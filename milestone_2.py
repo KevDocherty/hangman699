26f395553b7cca1ebb8c4fe69d231c329afe4265
Kevin Docherty <kevdocherty@gmail.com>
2023-11-13 16:47:15 +0000

    milestone2 complete

---
+++ milestone_2.py
@@ -0,0 +1,15 @@
+import random
+
+# create a list of 5 fruits...
+word_list = ['apple', 'banana', 'grape', 'peach', 'carrot']
+print(word_list)
+word = random.choice(word_list)
+print(word)
+
+guess = input("Please enter a single letter: ")
+if len(guess) == 1 and guess.isalpha:
+    print("Good guess!")
+else:
+    print("That's not a valid input.")
+
+#print(guess.isalpha())
