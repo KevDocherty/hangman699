b5d5f7838e701bf1e9ea62a6def4f453fbf0bfdd
Kevin Docherty <kevdocherty@gmail.com>
2023-11-15 16:23:53 +0000

    fixes a problem with indentation of an 'else' block in check_guess()

--- milestone_4.py
+++ milestone_4.py
@@ -6,40 +6,43 @@ class Hangman:
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(word_list)
-        #print(self.word)
+        print(self.word)
         self.word_guessed = ['_' for letter in self.word]
         self.num_letters = len(set(list(self.word)))
         self.list_of_guesses = []
 
     def check_guess(self, guess):
+        print('inside check_guess')
         self.guess = guess.lower()
+        print(f'self.guess = {self.guess}')
 
         if self.guess in self.word:
             print(f"Good guess! {self.guess} is in the word.")
-
-            for letter, index in enumerate(self.word):
+            for index, letter in enumerate(self.word):
                 if letter == self.guess:
                     self.word_guessed[index] = letter
-                else:
-                    self.num_lives -= 1
-                    print(f"Sorry, {letter} is not in the word.")
-                    print(f"You have {self.num_lives} lives left.")
+                    self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {self.guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
 
-            self.num_letters -= 1
+
+            print(self.word_guessed)
 
 
     def ask_for_input(self):
         # ask the user to guess a letter
         while True:
             self.guess = input("Please enter a single letter: ")
-            if len(self.guess) != 1 and not (guess.isalpha):
+            if len(self.guess) != 1 or not self.guess.isalpha():
                 print("Invalid letter. Please, enter a single alphabetical character.")
             elif self.guess in self.list_of_guesses:
                 print("You already tried that letter!")
             else:
                 self.check_guess(self.guess)
                 self.list_of_guesses.append(self.guess)
-                #print(self.list_of_guesses)
+                print(self.list_of_guesses)
 
 
 
